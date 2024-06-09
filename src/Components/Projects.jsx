b902556c445ef11321de6dd1b923675b219f9454
Sayed Hossain Sifat <sayedhossainsifat300@gmail.com>
2024-06-09 21:13:20 +0600

    major part are done

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,8 +3,6 @@ import project1 from "../assets/images/proj1.png";
 import project2 from "../assets/images/proj2.png";
 import project3 from "../assets/images/proj3.png";
 import project4 from "../assets/images/proj4.png";
-import project5 from "../assets/images/proj5.png";
-import project6 from "../assets/images/proj6.png";
 
 const Projects = () => {
   return (
@@ -14,71 +12,147 @@ const Projects = () => {
     >
       <h1
         data-aos="fade-right"
-        className="text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-600"
+        className="text-[52px] font-semibold mb-20
+         leading-normal uppercase text-fuchsia-600"
       >
         Projects :{" "}
       </h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-14">
-        <img
-          height={350}
-          width={300}
-          data-aos="fade-up"
-          className="text-[26px] flex justify-center items-center 
+        <div data-aos="fade-up">
+          <img
+            height={350}
+            width={300}
+            className="text-[26px] flex justify-center items-center 
         font-semibold text-fuchsia-800
          rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project1}
-          alt=""
-        />
-        <img
-          data-aos="fade-down"
-          height={350}
-          width={300}
-          className="text-[26px] flex justify-center items-center 
+            src={project1}
+            alt=""
+          />
+          <div className="flex justify-center items-center gap-2">
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Live Link
+            </a>
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Github Link
+            </a>
+          </div>
+        </div>
+
+        <div data-aos="fade-down">
+          <img
+            height={350}
+            width={300}
+            className="text-[26px] flex justify-center items-center 
         font-semibold text-fuchsia-800
          rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project2}
-          alt=""
-        />
-        <img
-          data-aos="fade-up"
-          height={350}
-          width={300}
-          className="text-[26px] flex justify-center items-center 
+            src={project2}
+            alt=""
+          />
+          <div className="flex justify-center items-center gap-2">
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Live Link
+            </a>
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Github Link
+            </a>
+          </div>
+        </div>
+
+        <div data-aos="fade-up">
+          <img
+            height={350}
+            width={300}
+            className="text-[26px] flex justify-center items-center 
         font-semibold text-fuchsia-800
          rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project3}
-          alt=""
-        />
-        <img
-          data-aos="fade-down"
-          height={350}
-          width={300}
-          className="text-[26px] flex justify-center items-center 
-        font-semibold text-fuchsia-800
-         rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project4}
-          alt=""
-        />
-        <img
-          data-aos="fade-up"
-          height={350}
-          width={300}
-          className="text-[26px] flex justify-center items-center 
-        font-semibold text-fuchsia-800
-         rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project5}
-          alt=""
-        />
-        <img
-          data-aos="fade-down"
-          height={350}
-          width={300}
-          className="text-[26px] flex justify-center items-center 
-        font-semibold text-fuchsia-800
-         rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
-          src={project6}
-          alt=""
-        />
+            src={project3}
+            alt=""
+          />
+          <div className="flex justify-center items-center gap-2">
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Live Link
+            </a>
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Github Link
+            </a>
+          </div>
+        </div>
+
+        <div data-aos="fade-up">
+          <img
+            height={350}
+            width={300}
+            className="text-[26px] flex justify-center items-center 
+            font-semibold text-fuchsia-800
+            rounded-3xl h-36 w-48 border-2 border-fuchsia-800 b_glow"
+            src={project4}
+            alt=""
+          />
+          <div className="flex justify-center items-center gap-2">
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Live Link
+            </a>
+            <a
+              className=" mt-4
+             bg-indigo-600 px-1 rounded
+             text-gray-400
+             hover:text-white	
+             "
+              href=""
+            >
+              Github Link
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
