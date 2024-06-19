c3debb28bca5aaec9bef81e6daa335e18ad66518
codeCampWithSifat <sayedhossainsifat300@.com>
2024-06-19 17:59:51 +0600

    fix are done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./Components/About";
-import Contact from "./Components/Contact";
+// import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
@@ -15,7 +15,7 @@ const App = () => {
       <Services />
       <Projects />
       {/* <Experiance /> */}
-      <Contact />
+      {/* <Contact /> */}
       <Footer />
     </div>
   );
